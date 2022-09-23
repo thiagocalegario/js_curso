bcc2645e7be2fb7afd322cd7579466a917f370ba
Thiago - IBM <IBM@MacBook-Pro-de-Thiago.local>
2022-09-23 07:31:40 -0300

    while_loop demonstration

---
+++ while_loop.js
@@ -0,0 +1,16 @@
+
+//Simple while loop
+let i = 0
+while(i < 5){
+    console.log(i)
+    i++
+}
+
+//Do while loop
+
+//While loop sempre será executado uma vez
+let j = 10
+do{
+    console.log(j)
+    j++
+}while (j<20)
